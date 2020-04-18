46d8e0c386488c7235453fdab51f839afc8ebc93
KyrgyzDiamond <joldoshbekov.almazbek@gmail.com>
2020-04-18 15:37:50 +0600

    appjs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from '../src/screens/dashboard';
+import Contact from '../src/screens/contact';
+
+function App() {
+  return (
+      <div>
+        <Dashboard />
+        <Contact />
+      </div>
+  );
+}
+
+export default App;
