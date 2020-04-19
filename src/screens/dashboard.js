61689225ef3aef6cfca9c7fdd4d022f30b978aa8
KyrgyzDiamond <joldoshbekov.almazbek@gmail.com>
2020-04-19 14:31:23 +0600

    changed styles

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 function Dashboard() {
     return (
-        <div className={"wrapper"}>
-            <div className={"item"}>
-                <img src={require("../images/img.jpg")} alt=""/>
-                <p>lore lorem lorem lorem </p>
-            </div>
-            <div className={"item"}>
-                <img src={require("../images/img.jpg")} alt=""/>
-                <p>lore lorem lorem lorem </p>
-            </div>
-            <div className={"item"}>
-                <img src={require("../images/img.jpg")} alt=""/>
-                <p>lore lorem lorem lorem </p>
-            </div>
+        <section>
+            <div className={"wrapper"}>
+                <div className={"item"}>
+                    <img src={require("../images/img.jpg")} alt=""/>
+                    <p>lore lorem lorem lorem </p>
 
-        </div>
+                </div>
+                <div className={"item"}>
+                    <img src={require("../images/img.jpg")} alt=""/>
+                    <p>lore lorem lorem lorem </p>
+                </div>
+                <div className={"item"}>
+                    <img src={require("../images/img.jpg")} alt=""/>
+                    <p>lore lorem lorem lorem </p>
+                </div>
+            </div>
+        </section>
     );
 }
 
